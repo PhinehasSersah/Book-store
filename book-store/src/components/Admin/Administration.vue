51c1535158ad6ad77170d22fc397ceea5c8639d6
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-09-15 21:15:26 +0000

    refactored edit component

--- book-store/src/components/Admin/Administration.vue
+++ book-store/src/components/Admin/Administration.vue
@@ -5,7 +5,6 @@
     <div class="w-full flex">
       <div class="w-1/2 border-r-[1px] border-dark">
         <h3 class="text-center text-xl text-dark my-3">Add New Book</h3>
-
         <form class="w-3/5 mx-auto shadow-md rounded p-4">
           <div class="mb-3">
             <label
@@ -15,10 +14,11 @@
             >
             <input
               type="text"
-              id="email"
+              id="title"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
               placeholder="Harry Porter"
               required
+              v-model="createBookData.title"
             />
           </div>
           <div class="mb-3">
@@ -33,6 +33,7 @@
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
               placeholder="30"
               required
+              v-model="createBookData.quantity"
             />
           </div>
           <div class="mb-3">
@@ -47,6 +48,7 @@
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
               placeholder="400"
               required
+              v-model="createBookData.price"
             />
           </div>
           <div class="mb-3">
@@ -59,7 +61,10 @@
               type="file"
               id="image"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-              required=""
+              required
+              v-on:change="handleImageChange($event)"
+              accept="image/*"
+              capture
             />
           </div>
           <div class="mb-3">
@@ -73,6 +78,7 @@
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
               required
               placeholder="Describe the book"
+              v-model="createBookData.description"
             >
             </textarea>
           </div>
@@ -88,93 +94,7 @@
       <div class="w-1/2">
         <h3 class="text-center text-xl text-dark my-3">Edit Existing Books</h3>
         <div class="w-full h-[60vh] overflow-y-auto">
-          <form
-            class="w-3/5 mx-auto shadow-lg shadow-brown rounded-lg p-12 my-16"
-          >
-            <div class="mb-2">
-              <label
-                for="title"
-                class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Book Title</label
-              >
-              <input
-                type="text"
-                id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-                placeholder="Harry Porter"
-                required
-              />
-            </div>
-            <div class="mb-2">
-              <label
-                for="quantity"
-                class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Stock Quantity</label
-              >
-              <input
-                type="number"
-                id="quantity"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-                placeholder="30"
-                required
-              />
-            </div>
-            <div class="mb-2">
-              <label
-                for="price"
-                class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Price</label
-              >
-              <input
-                type="number"
-                id="price"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-                placeholder="400"
-                required
-              />
-            </div>
-            <div class="mb-2">
-              <label
-                for="image"
-                class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Picture</label
-              >
-              <input
-                type="file"
-                id="image"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-                required=""
-              />
-            </div>
-            <div class="mb-2">
-              <label
-                for="description"
-                class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Description</label
-              >
-              <textarea
-                id="description"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
-                required
-                placeholder="Describe the book"
-              >
-              </textarea>
-            </div>
-            <div class="flex justify-between mt-4">
-              <button
-                type="submit"
-                class="text-white bg-dark hover:bg-light hover:text-dark hover:border-2 hover:border-dark focus:ring-4 focus:outline-none focus:ring-dark font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark"
-              >
-                Submit
-              </button>
-              <p
-                type="submit"
-                class="text-white bg-red-600 hover:bg-light hover:text-red-600 hover:border-2 hover:border-red-600 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark"
-              >
-                Delete
-              </p>
-            </div>
-          </form>
+          <EditBook />
         </div>
       </div>
     </div>
@@ -183,4 +103,21 @@
 
 <script setup>
 import Hearder from "../Home/Hearder.vue";
+import { reactive, ref } from "vue";
+import EditBook from "./EditBook.vue";
+
+// create books
+const createBookData = reactive({
+  title: "",
+  quantity: "",
+  price: "",
+  description: "",
+});
+const pictureData = ref();
+const handleImageChange = ($event) => {
+  let target = $event.target;
+  if (target && target.files) {
+    pictureData.value = target.files[0];
+  }
+};
 </script>
---
+++ book-store/src/components/Admin/EditBook.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <form class="w-3/5 mx-auto shadow-lg shadow-brown rounded-lg p-12 my-16">
+      <div class="mb-2">
+        <label
+          for="title"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Book Title</label
+        >
+        <input
+          type="text"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
+          placeholder="Harry Porter"
+          required
+          v-model="editBookData.title"
+        />
+      </div>
+      <div class="mb-2">
+        <label
+          for="quantity"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Stock Quantity</label
+        >
+        <input
+          type="number"
+          id="quantity"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
+          placeholder="30"
+          required
+          v-model="editBookData.quantity"
+        />
+      </div>
+      <div class="mb-2">
+        <label
+          for="price"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Price</label
+        >
+        <input
+          type="number"
+          id="price"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
+          placeholder="400"
+          required
+          v-model="editBookData.price"
+        />
+      </div>
+      <div class="mb-2">
+        <label
+          for="image"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Picture</label
+        >
+        <input
+          type="file"
+          id="image"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
+          required
+          @change="handleEditImageChange($event)"
+        />
+      </div>
+      <div class="mb-2">
+        <label
+          for="description"
+          class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >Description</label
+        >
+        <textarea
+          id="description"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark focus:border-dark block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark dark:focus:border-dark"
+          required
+          placeholder="Describe the book"
+          v-model="editBookData.description"
+        >
+        </textarea>
+      </div>
+      <div class="flex justify-between mt-4">
+        <button
+          type="submit"
+          class="text-white bg-dark hover:bg-light hover:text-dark hover:border-2 hover:border-dark focus:ring-4 focus:outline-none focus:ring-dark font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark"
+        >
+          Submit
+        </button>
+        <p
+          type="submit"
+          class="text-white bg-red-600 hover:bg-light hover:text-red-600 hover:border-2 hover:border-red-600 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-dark dark:hover:bg-dark dark:focus:ring-dark"
+        >
+          Delete
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+// Editing books
+
+const editBookData = reactive({
+  title: "",
+  quantity: "",
+  price: "",
+  description: "",
+});
+const editPictureData = ref();
+const handleEditImageChange = ($event) => {
+  let target = $event.target;
+  if (target && target.files) {
+    editPictureData.value = target.files[0];
+  }
+};
+</script>
